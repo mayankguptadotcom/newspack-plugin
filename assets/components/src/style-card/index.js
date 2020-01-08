cb50de1d8d320c8b8f23c06ea19982b3eaef111e
Thomas Guillot <thomasguillot@users.noreply.github.com>
2020-01-08 17:09:54 +0000

    Style Card: Use Modal and ResponsiveWrapper for the demo

--- assets/components/src/style-card/index.js
+++ assets/components/src/style-card/index.js
@@ -6,13 +6,13 @@
  * WordPress dependencies.
  */
 import { Component } from '@wordpress/element';
-// import { DropdownMenu } from '@wordpress/components';
+import { ResponsiveWrapper } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
-import { Button } from '../';
+import { Button, Card, Modal } from '../';
 import './style.scss';
 
 /**
@@ -22,10 +22,37 @@ import classnames from 'classnames';
 
 class StyleCard extends Component {
 	/**
+	 * Constructor.
+	 */
+	constructor() {
+		super( ...arguments );
+		this.state = {
+			modalShown: false,
+			width: 0,
+			height: 0,
+		};
+		this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+	}
+
+	componentDidMount() {
+	  this.updateWindowDimensions();
+	  window.addEventListener('resize', this.updateWindowDimensions);
+	}
+
+	componentWillUnmount() {
+	  window.removeEventListener('resize', this.updateWindowDimensions);
+	}
+
+	updateWindowDimensions() {
+	  this.setState({ width: window.innerWidth, height: window.innerHeight });
+	}
+
+	/**
 	 * Render.
 	 */
 	render( props ) {
 		const { className, cardTitle, demo, image, isActive, onClick, ...otherProps } = this.props;
+		const { modalShown, width, height } = this.state;
 		const classes = classnames(
 			'newspack-style-card',
 			isActive && 'newspack-style-card__is-active',
@@ -37,7 +64,15 @@ class StyleCard extends Component {
 				{ image && <img src={ image } /> }
 					<div className="newspack-style-card__actions">
 						{ ! isActive && <Button isPrimary isSmall onClick={ onClick }>{ __( 'Activate' ) }</Button> }
-						{ demo && <Button isDefault isSmall href={ demo } target="_blank">{ __( 'View Demo' ) }</Button>	}
+						{ demo &&
+							<Button
+								isDefault
+								isSmall
+								onClick={ () => this.setState( { modalShown: true } ) }
+							>
+								{ __( 'View Demo' ) }
+							</Button>
+						}
 					</div>
 				</div>
 				<div className="newspack-style-card__content">
@@ -46,6 +81,22 @@ class StyleCard extends Component {
 						{ isActive && <span className="newspack-style-card__status">{ __( 'Active' ) }</span> }
 					</h2>
 				</div>
+				{ modalShown && (
+					<Modal
+						onRequestClose={ () => this.setState( { modalShown: false } ) }
+						isWide
+						className="newspack-modal__style-card"
+					>
+						<Card noBackground isWide>
+							<ResponsiveWrapper
+								naturalWidth={ width }
+								naturalHeight={ height }
+							>
+								<iframe src={ demo }></iframe>
+							</ResponsiveWrapper>
+						</Card>
+					</Modal>
+				) }
 			</div>
 		);
 	}
