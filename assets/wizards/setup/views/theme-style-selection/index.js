46427e8ac017f4b7d6c838afad2c0f1d00e5eef7
Thomas Guillot <thomasguillot@users.noreply.github.com>
2020-01-08 17:16:13 +0000

    Theme Style Selection: Add temporary demo URLs

--- assets/wizards/setup/views/theme-style-selection/index.js
+++ assets/wizards/setup/views/theme-style-selection/index.js
@@ -35,36 +35,42 @@ class ThemeStyleSelection extends Component {
 					image={ ScreenshotStyle0 }
 					isActive={ themeStyle === 'default' }
 					onClick={ () => updateThemeStyle( 'default' ) }
+					demo="https://dalstondemo.wordpress.com/?demo"
 				/>
 				<StyleCard
 					cardTitle={ __( 'Style 1', 'newspack-plugin' ) }
 					image={ ScreenshotStyle1 }
 					isActive={ themeStyle === 'style-1' }
 					onClick={ () => updateThemeStyle( 'style-1' ) }
+					demo="https://maylanddemo.wordpress.com/?demo"
 				/>
 				<StyleCard
 					cardTitle={ __( 'Style 2', 'newspack-plugin' ) }
 					image={ ScreenshotStyle2 }
 					isActive={ themeStyle === 'style-2' }
 					onClick={ () => updateThemeStyle( 'style-2' ) }
+					demo="https://balasanademo.wordpress.com/?demo"
 				/>
 				<StyleCard
 					cardTitle={ __( 'Style 3', 'newspack-plugin' ) }
 					image={ ScreenshotStyle3 }
 					isActive={ themeStyle === 'style-3' }
 					onClick={ () => updateThemeStyle( 'style-3' ) }
+					demo="https://exforddemo.wordpress.com/?demo"
 				/>
 				<StyleCard
 					cardTitle="Sacha"
 					image={ ScreenshotSacha }
 					isActive={ themeStyle === 'style-4' }
 					onClick={ () => updateThemeStyle( 'style-4' ) }
+					demo="https://barnsburydemo.wordpress.com/?demo"
 				/>
 				<StyleCard
 					cardTitle={ __( 'Style 5', 'newspack-plugin' ) }
 					image={ ScreenshotStyle5 }
 					isActive={ themeStyle === 'style-5' }
 					onClick={ () => updateThemeStyle( 'style-5' ) }
+					demo="https://rivingtondemo.wordpress.com/?demo"
 				/>
 			</StyleCardGroup>
 		);
